6bcba54662820d2f93f58acb8d1130ff7abc71a5
wuxii <wuxii@foxmail.com>
2015-07-29 01:11:20 +0800

    提交 web javaee容器贯穿测试
    Signed-off-by: wuxii <wuxii@foxmail.com>

---
+++ examples/example-war/src/main/java/com/harmony/umbrella/examples/web/filter/ApplicationWebFilter.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2002-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.harmony.umbrella.examples.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.harmony.umbrella.context.ApplicationContext;
+import com.harmony.umbrella.context.DefaultHttpCurrentContext;
+
+/**
+ * @author wuxii@foxmail.com
+ */
+@WebFilter(urlPatterns = "/*")
+public class ApplicationWebFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        ApplicationContext.getApplicationContext().checkIn(new DefaultHttpCurrentContext(req, resp));
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
