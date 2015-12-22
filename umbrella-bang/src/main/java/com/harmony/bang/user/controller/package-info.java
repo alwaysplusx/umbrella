0c954912ebb514a4d70b077f205161c8ec0db2aa
wuxii <wuxii@foxmail.com>
2015-12-22 23:50:11 +0800

    delete umbrella-bang, move resources to umbrella-web

--- umbrella-bang/src/main/java/com/harmony/bang/user/controller/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * @author wuxii@foxmail.com
- */
-package com.harmony.bang.user.controller;
--- umbrella-bang/src/main/webapp/resources/bang/user/store/UserStore.js
+++
@@ -1,7 +0,0 @@
-Ext.define("Q.user.store.UserStore", {
-    extend: "Ext.bang.data.ListStore",
-    model: "Q.user.model.UserModel",
-    autoLoad: Q.isAutoLoad(),
-    pageSize: 20,
-    url: "user/page"
-});
