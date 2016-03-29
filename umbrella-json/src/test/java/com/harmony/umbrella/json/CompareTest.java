c072e181b994e5793191b1108e105eb85fe45959
wuxii <wuxii@foxmail.com>
2016-03-30 00:04:19 +0800

    fastjson与jackson的比较选型
    Signed-off-by: wuxii <wuxii@foxmail.com>

---
+++ umbrella-json/src/test/java/com/harmony/umbrella/json/CompareTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.harmony.umbrella.json;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.Test;
+
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.harmony.umbrella.json.vo.Foo;
+
+/**
+ * @author wuxii@foxmail.com
+ */
+public class CompareTest {
+
+    private static final int count = 1000000;
+    private static final List<Foo> foos = new ArrayList<Foo>();
+
+    static {
+        for (int i = 0; i < count; i++) {
+            foos.add(newFoo());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // new CompareTest().testFastjson();
+        new CompareTest().testJackson();
+    }
+
+    public void testJackson() throws IOException {
+        ObjectMapper om = new ObjectMapper();
+        long startTime = System.currentTimeMillis();
+        for (Foo foo : foos) {
+            om.writeValue(new StringWriter(), foo);
+        }
+        System.out.println("Jackson use:" + (System.currentTimeMillis() - startTime) + "ms");
+    }
+
+    @Test
+    public void testFastjson() {
+        long startTime = System.currentTimeMillis();
+        for (Foo foo : foos) {
+            JSON.toJSONString(foo);
+        }
+        System.out.println("Fastjson use:" + (System.currentTimeMillis() - startTime) + "ms");
+    }
+
+    private static Foo newFoo() {
+        Foo foo = new Foo();
+        foo.setA(UUID.randomUUID().toString());
+        foo.setB((int) Math.random());
+        foo.setC((char) Math.random());
+        foo.setD((short) Math.random());
+        foo.setE(new Date());
+        foo.setF(Calendar.getInstance());
+        foo.setG(Math.round(Math.random()));
+        foo.setH((float) Math.random());
+        foo.setI(Math.random());
+        foo.setJ(BigDecimal.valueOf(Math.random()));
+        foo.setK(Math.round(Math.random()));
+        foo.setL((int) Math.round(Math.random()));
+        foo.setM(true);
+        foo.setN(UUID.randomUUID().toString());
+        foo.setO(UUID.randomUUID().toString());
+        foo.setP(UUID.randomUUID().toString());
+        foo.setQ(UUID.randomUUID().toString());
+        foo.setR(UUID.randomUUID().toString());
+        foo.setS((int) Math.round(Math.random()));
+        foo.setT((int) Math.round(Math.random()));
+        foo.setU((int) Math.round(Math.random()));
+        foo.setV((int) Math.round(Math.random()));
+        foo.setW((int) Math.round(Math.random()));
+        foo.setX(new Timestamp(System.currentTimeMillis()));
+        foo.setY(UUID.randomUUID().toString());
+        foo.setZ(new Timestamp(System.currentTimeMillis()));
+        return foo;
+    }
+
+}
---
+++ umbrella-json/src/test/java/com/harmony/umbrella/json/vo/Foo.java
@@ -0,0 +1,263 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.harmony.umbrella.json.vo;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author wuxii@foxmail.com
+ */
+public class Foo {
+
+    private String a;
+    private int b;
+    private char c;
+    private short d;
+    private Date e;
+    private Calendar f;
+    private long g;
+    private float h;
+    private double i;
+    private BigDecimal j;
+    private Long k;
+    private Integer l;
+    private boolean m;
+    private String n;
+    private String o;
+    private String p;
+    private String q;
+    private String r;
+    private Integer s;
+    private Integer t;
+    private Integer u;
+    private Integer v;
+    private Integer w;
+    private Timestamp x;
+    private String y;
+    private Timestamp z;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public char getC() {
+        return c;
+    }
+
+    public void setC(char c) {
+        this.c = c;
+    }
+
+    public short getD() {
+        return d;
+    }
+
+    public void setD(short d) {
+        this.d = d;
+    }
+
+    public Date getE() {
+        return e;
+    }
+
+    public void setE(Date e) {
+        this.e = e;
+    }
+
+    public Calendar getF() {
+        return f;
+    }
+
+    public void setF(Calendar f) {
+        this.f = f;
+    }
+
+    public long getG() {
+        return g;
+    }
+
+    public void setG(long g) {
+        this.g = g;
+    }
+
+    public float getH() {
+        return h;
+    }
+
+    public void setH(float h) {
+        this.h = h;
+    }
+
+    public double getI() {
+        return i;
+    }
+
+    public void setI(double i) {
+        this.i = i;
+    }
+
+    public BigDecimal getJ() {
+        return j;
+    }
+
+    public void setJ(BigDecimal j) {
+        this.j = j;
+    }
+
+    public Long getK() {
+        return k;
+    }
+
+    public void setK(Long k) {
+        this.k = k;
+    }
+
+    public Integer getL() {
+        return l;
+    }
+
+    public void setL(Integer l) {
+        this.l = l;
+    }
+
+    public boolean isM() {
+        return m;
+    }
+
+    public void setM(boolean m) {
+        this.m = m;
+    }
+
+    public String getN() {
+        return n;
+    }
+
+    public void setN(String n) {
+        this.n = n;
+    }
+
+    public String getO() {
+        return o;
+    }
+
+    public void setO(String o) {
+        this.o = o;
+    }
+
+    public String getP() {
+        return p;
+    }
+
+    public void setP(String p) {
+        this.p = p;
+    }
+
+    public String getQ() {
+        return q;
+    }
+
+    public void setQ(String q) {
+        this.q = q;
+    }
+
+    public String getR() {
+        return r;
+    }
+
+    public void setR(String r) {
+        this.r = r;
+    }
+
+    public Integer getS() {
+        return s;
+    }
+
+    public void setS(Integer s) {
+        this.s = s;
+    }
+
+    public Integer getT() {
+        return t;
+    }
+
+    public void setT(Integer t) {
+        this.t = t;
+    }
+
+    public Integer getU() {
+        return u;
+    }
+
+    public void setU(Integer u) {
+        this.u = u;
+    }
+
+    public Integer getV() {
+        return v;
+    }
+
+    public void setV(Integer v) {
+        this.v = v;
+    }
+
+    public Integer getW() {
+        return w;
+    }
+
+    public void setW(Integer w) {
+        this.w = w;
+    }
+
+    public Timestamp getX() {
+        return x;
+    }
+
+    public void setX(Timestamp x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+
+    public Timestamp getZ() {
+        return z;
+    }
+
+    public void setZ(Timestamp z) {
+        this.z = z;
+    }
+
+}
