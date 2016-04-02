d125ebca49737d1c474ab14d8a84d0c3e2cf8ce5
wuxii <wuxii@foxmail.com>
2016-04-02 13:47:14 +0800

    zip utils
    Signed-off-by: wuxii <wuxii@foxmail.com>

--- umbrella-core/src/main/java/com/harmony/umbrella/util/ZipUtils.java
+++ umbrella-core/src/main/java/com/harmony/umbrella/util/ZipUtils.java
@@ -16,52 +16,100 @@ import java.util.zip.ZipOutputStream;
  */
 public class ZipUtils {
 
-    public static void zip(File file, File dest) {
+    // do dest
 
+    public static void zip(String src, String dest) throws IOException {
+        zip(new File(src), new File(dest));
     }
 
-    public static void zip(File file, FileFilter filter, File dest) {
+    public static void zip(String src, FileFilter filter, String dest) throws IOException {
+        zip(new File(src), filter, new File(dest));
+    }
 
+    public static void zip(String[] src, FileFilter filter, String dest) throws IOException {
+        zip(toFiles(src), filter, new File(dest));
+    }
+
+    public static void zip(File file, File dest) throws IOException {
+        zipToDest(new File[] { file }, null, dest);
+    }
+
+    public static void zip(File file, FileFilter filter, File dest) throws IOException {
+        zipToDest(new File[] { file }, filter, dest);
     }
 
     public static void zip(File[] files, FileFilter filter, File dest) throws IOException {
-        doZip(files, filter, dest);
+        zipToDest(files, filter, dest);
     }
 
-    private static void doZip(File[] files, FileFilter filter, File dest) throws IOException {
+    private static void zipToDest(File[] files, FileFilter filter, File dest) throws IOException {
         ZipOutputStream zos = toZipOutputStream(new FileOutputStream(dest));
-        doZip(files, filter, zos);
+        zipToOutStream(files, filter, zos);
         zos.close();
     }
 
+    // to byte
+
+    public static byte[] zip(String... pathnames) throws IOException {
+        return zip(toFiles(pathnames));
+    }
+
+    public static byte[] zip(String[] pathnames, FileFilter filter) throws IOException {
+        return zip(toFiles(pathnames), filter);
+    }
+
     public static byte[] zip(File... files) throws IOException {
+        return zipToByte(files, null);
+    }
+
+    public static byte[] zip(File[] files, FileFilter filter) throws IOException {
+        return zipToByte(files, filter);
+    }
+
+    private static byte[] zipToByte(File[] files, FileFilter filter) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        for (File file : files) {
-            zip(file, baos);
-        }
+        ZipOutputStream zos = toZipOutputStream(baos);
+        zipToOutStream(files, filter, zos);
         return baos.toByteArray();
     }
 
+    // zip to outstream
+    public static void zip(String pathname, OutputStream os) throws IOException {
+        zip(new File(pathname), os);
+    }
+
+    public static void zip(String[] pathnames, OutputStream os) throws IOException {
+        zip(toFiles(pathnames), os);
+    }
+
+    public static void zip(String pathname, FileFilter filter, OutputStream os) throws IOException {
+        zip(new File(pathname), filter, os);
+    }
+
+    public static void zip(String[] pathnames, FileFilter filter, OutputStream os) throws IOException {
+        zip(toFiles(pathnames), filter, os);
+    }
+
     public static void zip(File file, OutputStream os) throws IOException {
-        doZip(new File[] { file }, null, toZipOutputStream(os));
+        zipToOutStream(new File[] { file }, null, toZipOutputStream(os));
     }
 
     public static void zip(File[] files, OutputStream os) throws IOException {
-        doZip(files, null, toZipOutputStream(os));
+        zipToOutStream(files, null, toZipOutputStream(os));
     }
 
     public static void zip(File file, FileFilter filter, OutputStream os) throws IOException {
-        doZip(new File[] { file }, filter, toZipOutputStream(os));
+        zipToOutStream(new File[] { file }, filter, toZipOutputStream(os));
     }
 
     public static void zip(File[] files, FileFilter filter, OutputStream os) throws IOException {
-        doZip(files, filter, toZipOutputStream(os));
+        zipToOutStream(files, filter, toZipOutputStream(os));
     }
 
-    private static void doZip(File[] files, FileFilter filter, ZipOutputStream zos) throws IOException {
+    private static void zipToOutStream(File[] files, FileFilter filter, ZipOutputStream zos) throws IOException {
         for (File file : files) {
             if (file.isDirectory()) {
-                doZip(file.listFiles(), filter, zos);
+                zipToOutStream(file.listFiles(), filter, zos);
             } else if (filter == null || filter.accept(file)) {
                 zipSingleFile(file, zos);
             }
@@ -69,6 +117,15 @@ public class ZipUtils {
     }
 
     //
+
+    private static File[] toFiles(String... pathnames) {
+        File[] files = new File[pathnames.length];
+        for (int i = 0; i < pathnames.length; i++) {
+            files[i] = new File(pathnames[i]);
+        }
+        return files;
+    }
+
     private static ZipOutputStream toZipOutputStream(OutputStream os) {
         return os instanceof ZipOutputStream ? (ZipOutputStream) os : new ZipOutputStream(os);
     }
@@ -82,11 +139,6 @@ public class ZipUtils {
     private static void zip(String entryName, InputStream is, ZipOutputStream zos) throws IOException {
         zos.putNextEntry(new ZipEntry(entryName));
         IOUtils.copy(is, zos);
-    }
-
-    public static void main(String[] args) throws IOException {
-        FileOutputStream fos = new FileOutputStream("a.zip");
-        ZipUtils.zip(new File("target"), null, fos);
     }
 
 }
