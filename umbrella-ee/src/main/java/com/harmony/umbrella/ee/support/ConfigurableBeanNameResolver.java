9a366e2b4efe95be284ed1836696f9c5c59903f2
wuxii <wuxii@foxmail.com>
2016-08-01 18:33:34 +0800

    just commit
    Signed-off-by: wuxii <wuxii@foxmail.com>

--- umbrella-ee/src/main/java/com/harmony/umbrella/ee/support/ConfigurableBeanNameResolver.java
+++ umbrella-ee/src/main/java/com/harmony/umbrella/ee/support/ConfigurableBeanNameResolver.java
@@ -1,39 +1,64 @@
 package com.harmony.umbrella.ee.support;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.naming.Context;
 
 import com.harmony.umbrella.ee.BeanDefinition;
+import com.harmony.umbrella.util.StringUtils;
 
 /**
  * @author wuxii@foxmail.com
  */
 public class ConfigurableBeanNameResolver extends AbstractBeanNameResolver {
 
-    private Set<String> beanNameAttributes;
+    private List<String> beanNameAttributes;
 
-    private Set<String> jndiAttributes;
+    private List<String> jndiAttributes;
 
     @Override
     protected String[] guessNames(BeanDefinition bd, Map<String, Object> properties, Context context) {
+        List<String> jndis = new ArrayList<String>();
+        for (String attr : jndiAttributes) {
+            Object jndi = properties.get(attr);
+            if (jndi != null && jndi instanceof String && StringUtils.isNotBlank((String) jndi)) {
+                jndis.add((String) jndi);
+            }
+        }
+        if (!jndis.isEmpty()) {
+            // 属性中有直接配置jndi
+            return jndis.toArray(new String[jndis.size()]);
+        }
+
+        // 配置属性中的beanName
+        Collection<String> beanNames = new HashSet<String>();
+        for (String attr : beanNameAttributes) {
+            Object beanName = properties.get(attr);
+            if (beanName != null && beanName instanceof String && StringUtils.isNotBlank((String) beanName)) {
+                beanNames.add((String) beanName);
+            }
+        }
+        
         return null;
     }
 
-    public Set<String> getBeanNameAttributes() {
+    public List<String> getBeanNameAttributes() {
         return beanNameAttributes;
     }
 
-    public void setBeanNameAttributes(Set<String> beanNameAttributes) {
+    public void setBeanNameAttributes(List<String> beanNameAttributes) {
         this.beanNameAttributes = beanNameAttributes;
     }
 
-    public Set<String> getJndiAttributes() {
+    public List<String> getJndiAttributes() {
         return jndiAttributes;
     }
 
-    public void setJndiAttributes(Set<String> jndiAttributes) {
+    public void setJndiAttributes(List<String> jndiAttributes) {
         this.jndiAttributes = jndiAttributes;
     }
 
